d89a62cf3b88c2c1c74848912cfeef70e28dbf1d
Andres Montoya <montoyandres333@gmail.com>
2025-11-25 22:28:47 -0500

    Implement Quote Struct
    Quote struct holds Price and LastUpdated data
    
    Concrete classes Stock & Etf no longer have fields that are in Abstract class
    
    PortfolioItem now manages Quote so child classes don't have to

--- Stock.cs
+++ Stock.cs
@@ -2,18 +2,12 @@ namespace Financial_Portfolio_Manager;
 
 public class Stock : PortfolioItem
 {
-    private string _ticker { get; set; }
     private string _companyName { get; set; }
-    private int _price { get; set; }
     private string _sector { get; set; }
 
-    // need to add StockQuote struct later
-
-    public Stock(string ticker, string companyName, int price, string sector)
+    public Stock(string ticker, string companyName, int price, string sector) : base(ticker, price)
     {
-        _ticker = ticker;
         _companyName = companyName;
-        _price = price;
         _sector = sector;
     }
 }
