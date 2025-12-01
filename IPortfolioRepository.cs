2cccaf5c12653ebebbb94bb275395f685d49fcbe
Justin <Justin@beitay.com>
2025-11-30 19:50:48 -0500

    Implement Portfolio Service
    Implemented portfolio service methods

--- IPortfolioRepository.cs
+++ IPortfolioRepository.cs
@@ -5,4 +5,5 @@ public interface IPortfolioRepository
     List<Portfolio> GetAll();
     Portfolio GetById(int portfolioID);
     void Add(Portfolio portfolio);
+    void Delete(int portfolioId);
 }
